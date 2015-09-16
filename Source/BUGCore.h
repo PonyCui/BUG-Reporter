11b0f83093160f6b897f54f5139481a6bf58f518
ponycui <PonyCui_Home@saitekiiMac.local>
2015-09-17 00:41:44 +0800

    update callback url

--- Source/BUGCore.h
+++ Source/BUGCore.h
@@ -14,7 +14,7 @@
 
 #define WORKTILE_APPKEY @"330fa04d407848db935a0c106034b9a5"
 #define WORKTILE_APPSECRET @"9cae921e877b47419efcad4dbcdcd27b"
-#define WORKTILE_CALLBACK_URL @"http://bug.snh48.com/response"
+#define WORKTILE_CALLBACK_URL @"https://github.com/PonyCui/BUG-Reporter/response"
 
 @interface BUGCore : NSObject
 
